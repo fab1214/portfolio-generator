95ffe398f9aaed7c13a97a3864d3449de6775b8f
Fabricio <bustamantef1214@gmail.com>
2021-10-08 19:48:14 -0400

    add questions

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-//to use modules, use require first
-const fs = require('fs');
-//pull function from page-template and assign to a const variable
-const generatePage = require('./src/page-template.js');
-//start indexing from 2 in process.argv array 
-const profileDataArgs = process.argv.slice(2);
-//create const variable array equal to process.argv array
-const [name, github] = profileDataArgs;
-//create const variable to hold generatePage function with name, github parameters
-const pageHTML = generatePage(name, github);
+const inquirer = require('inquirer');
+// //to use modules, use require first
+// const fs = require('fs');
+// //pull function from page-template and assign to a const variable
+// const generatePage = require('./src/page-template.js');
+// //create const variable to hold generatePage function with name, github parameters
+// const pageHTML = generatePage(name, github);
 
-//write the file (fs.writeFile(file, data[options], error callback)
-fs.writeFile('./index.html', pageHTML, err => {
-    if(err) throw new Error(err);
+// //write the file (fs.writeFile(file, data[options], error callback)
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if(err) throw err;
 
-    console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }, 
+    {
+    type: 'checkbox',
+    name: 'toppings',
+    message: 'Choose your favorite pizza topping.',
+    choices: ['pepperoni', 'olives', 'jalapenos', 'bacon', 'asiago cheese']
+    }
+])
+.then(answers => console.log(answers));
